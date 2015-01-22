de271886bbff5622cfdd3230b87a2e62164bb37d
Meth <zj45499@gmail.com>
2015-01-22 14:27:30 +0800

    added percolation testclient

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,12 +1,27 @@
 package percolation;
 
+
+import edu.princeton.cs.algs4.UF;
+import edu.princeton.cs.introcs.StdRandom;
+
 /**
  * This file is created by @Muffin_C on 1/21/15 20:55.
  * This file is part of Project algs4partI.
  */
 public class Percolation {
 
+    private UF disjointSet;
+    private int N;
+    private int RAND = StdRandom.uniform(N);
+
     public Percolation(int N) { // create N-by-N grid, with all sites blocked
+        this.N = N;
+        disjointSet = new UF(N * N + 2);  // [N * N] as top node, [N * N + 1] as bottom node
+
+        for (int i = 0; i < N; i++) {
+            disjointSet.union(i, N * N);
+            int j = i +
+        }
 
     }
 
